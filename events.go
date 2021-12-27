a4f34dc7d8203f5ec0bd9e6c339af04c26d8a65d
Stanislav Vasilev <4ipsdaqde6@gmail.com>
2021-12-27 19:37:23 +0200

    Update events.go

--- events.go
+++ events.go
@@ -7,7 +7,7 @@ import (
 )
 
 func onReady(s *discordgo.Session, m *discordgo.Ready) {
-	err := s.UpdateStreamingStatus(1, "&help | Click watch for music!", "https://www.youtube.com/watch?v=hvfe_JPovW8&list=PL8yFU3veFghtteYYFdSnc-vaBZ3hHh4Wc&index=1")
+	err := s.UpdateStreamingStatus(1, "&help | Click watch for music!", "https://www.youtube.com/playlist?list=PL8yFU3veFghtteYYFdSnc-vaBZ3hHh4Wc")
 	if err != nil {
 		return
 	}
