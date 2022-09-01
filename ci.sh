e2d0c65dabcd85474a622d98cc3e358cbe61f186
Stanislav Vasilev <stanv.contact176@gmail.com>
2022-09-01 15:31:57 +0300

    Update ci.sh

--- ci.sh
+++ ci.sh
@@ -2,5 +2,6 @@
 vertmp=$(grep "github.com/MadLadSquad/discordgo" go.mod)
 ver=${vertmp: -8}
 
+go mod download github.com/MadLadSquad/discordgo
 go get github.com/MadLadSquad/discordgo@"${ver}"
 go build .
